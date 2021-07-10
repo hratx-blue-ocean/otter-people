b0235ff1b7c62b1d69be9821d8d86b42211d33e7
Felipe Erazo <felipe.erazoangarita@gmail.com>
2021-07-10 11:39:08 -0500

    BoilerPlate With Create React-App template using Chakra

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { ChakraProvider, Box, Text, VStack, Grid, theme, } from '@chakra-ui/react';
+import { ColorModeSwitcher } from './ColorModeSwitcher';
+
+function App() {
+  return (
+    <ChakraProvider theme={theme}>
+      <Box textAlign="center" fontSize="xl">
+        <Grid minH="100vh" p={3}>
+          <ColorModeSwitcher justifySelf="flex-end" />
+          <VStack spacing={8}>
+            <Text>
+              Hello world
+            </Text>
+          </VStack>
+        </Grid>
+      </Box>
+    </ChakraProvider>
+  );
+}
+
+export default App;
