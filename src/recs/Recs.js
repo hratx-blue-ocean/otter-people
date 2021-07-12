16e289e306a89cfa3811f4bd4819be65975f1f58
Angela Wu <ange.y.wu@gmail.com>
2021-07-12 16:54:18 -0500

    static layout done

--- src/recs/Recs.js
+++ src/recs/Recs.js
@@ -1,15 +1,17 @@
 import React from 'react';
 
 import { Box } from "@chakra-ui/react"
-import { Flex, Spacer } from "@chakra-ui/react"
 import { Text } from "@chakra-ui/react"
 import { Heading } from "@chakra-ui/react"
+import { Image } from "@chakra-ui/react"
+import { SimpleGrid } from "@chakra-ui/react"
+import { Select } from "@chakra-ui/react"
+import { Center } from "@chakra-ui/react"
+import { TriangleDownIcon } from '@chakra-ui/icons'
 
 import apiResult from './sampleData.js'
 
-// Sample card from Airbnb
-
-export default function Recs() {
+export default function Recs(props) {
 
   //useState hooks
 
@@ -18,39 +20,65 @@ export default function Recs() {
   return (
     <Box maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden">
       <Heading size="lg">Around Town</Heading>
-      <Text fontSize="sm">Click to Add As Your Next Event!</Text>
-      <Flex>
-        <Spacer />
-        <Box p="4" bg="red.400">
-          <Image src={property.imageUrl} alt={property.imageAlt} />
+      <Text fontSize="sm" >Click to Add As Your Next Event!</Text>
+      <Center>
+        <Select placeholder="Category" size="sm" width="50%">
+          <option value="option1">Sights</option>
+          <option value="option2">Nightlife</option>
+          <option value="option3">Restaurants</option>
+          <option value="option3">Shopping</option>
+        </Select>
+      </Center>
+
+      <SimpleGrid columns={2} >
+        <Box p="2" >
+          <Image src="https://www.calacademy.org/sites/default/files/styles/manual_crop_standard_960x540/public/uploads/images/cas22-155-layers-small.jpg?itok=_r3WtJWF&c=f14936ae55e8d4293886b19bcfa364bd"
+            alt="California Academy of Sciences" />
           <Box
             mt="1"
-            fontWeight="semibold"
-            as="h4"
             lineHeight="tight"
-            isTruncated
           >
-            {property.title}
+            <Text isTruncated fontSize="xs">California Academy of Sciences</Text>
           </Box>
         </Box>
-        <Spacer />
-        <Box p="4" bg="green.400">
-          Box 2
+        <Box p="2">
+          <Image src="https://www.calacademy.org/sites/default/files/styles/manual_crop_standard_960x540/public/uploads/images/cas22-155-layers-small.jpg?itok=_r3WtJWF&c=f14936ae55e8d4293886b19bcfa364bd"
+            alt="California Academy of Sciences" />
+          <Box
+            mt="1"
+            lineHeight="tight"
+          >
+            <Text isTruncated fontSize="xs">California Academy of Sciences</Text>
+          </Box>
         </Box>
-        <Spacer />
-      </Flex>
-      <br />
-      <Flex>
-        <Spacer />
-        <Box p="4" bg="red.400">
-          Box 1
+      </SimpleGrid>
+
+      <SimpleGrid columns={2} >
+        <Box p="2" >
+          <Image src="https://www.calacademy.org/sites/default/files/styles/manual_crop_standard_960x540/public/uploads/images/cas22-155-layers-small.jpg?itok=_r3WtJWF&c=f14936ae55e8d4293886b19bcfa364bd"
+            alt="California Academy of Sciences" />
+          <Box
+            mt="1"
+            lineHeight="tight"
+          >
+            <Text isTruncated fontSize="xs">California Academy of Sciences</Text>
+          </Box>
         </Box>
-        <Spacer />
-        <Box p="4" bg="green.400">
-          Box 2
+        <Box p="2">
+          <Image src="https://www.calacademy.org/sites/default/files/styles/manual_crop_standard_960x540/public/uploads/images/cas22-155-layers-small.jpg?itok=_r3WtJWF&c=f14936ae55e8d4293886b19bcfa364bd"
+            alt="California Academy of Sciences" />
+          <Box
+            mt="1"
+            lineHeight="tight"
+          >
+            <Text isTruncated fontSize="xs">California Academy of Sciences</Text>
+          </Box>
         </Box>
-        <Spacer />
-      </Flex>
+      </SimpleGrid>
+      <Center>
+        <TriangleDownIcon />
+        <br /><br />
+      </Center>
     </Box>
   )
 }
