e0c591827cf445b9b381712875f93f01d0f906b1
Addie Johnsen <79018160+addiejohnsen@users.noreply.github.com>
2021-07-12 11:29:01 -0500

    Groups Feature component draft

---
+++ src/Groups/AddGroupModal.js
@@ -0,0 +1,95 @@
+import React, { useState, useEffect } from 'react';
+import { useDisclosure } from "@chakra-ui/react";
+import { Input } from "@chakra-ui/react";
+import { Textarea } from "@chakra-ui/react";
+import { VStack } from "@chakra-ui/react";
+import { Button } from "@chakra-ui/react";
+import { FormLabel, FormControl } from '@chakra-ui/react';
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+} from "@chakra-ui/react";
+
+function AddGroupModal() {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const [groupName, setGroupName] = useState();
+  const [groupDescription, setGroupDescription] = useState();
+  const [groupPhoto, setGroupPhoto] = useState();
+
+  const handleGroupNameChange = (e) => {
+    let groupNameValue = e.target.value;
+    setGroupName(groupNameValue);
+  };
+
+  const handleGroupDescriptionChange = (e) => {
+    let groupDescriptionChange = e.target.value;
+    setGroupDescription(groupDescriptionChange);
+  };
+
+  const handleGroupPhotoUpload = (e) => {
+    let groupPhotoUpload = e.target.file;
+    setGroupPhoto(groupPhotoUpload);
+  };
+
+  const handleFormSubmission = (e) => {
+    console.log("cats")
+    onClose()
+    // HANDLE FORM SUBMISSION
+  }
+
+
+  return (
+    <>
+      <Button onClick={onOpen}>Create New Group</Button>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader >Create New Group</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <VStack spacing="20px">
+
+              <FormControl isRequired>
+                <FormLabel>Group Name</FormLabel>
+                <Input
+                  value={groupName}
+                  onChange={handleGroupNameChange}
+                  placeholder="Group Name"
+                />
+              </FormControl>
+              <FormControl isRequired>
+                <FormLabel>Group Description</FormLabel>
+                <Textarea
+                  value={groupDescription}
+                  onChange={handleGroupDescriptionChange}
+                  placeholder="Group Description"
+                  size="md"
+                  height="100px"
+                  resize="None"
+                />
+              </FormControl>
+              {/* <FormControl>
+              <FormLabel>Upload Group Photo</FormLabel>
+              <Input type="file" />
+              </FormControl> */}
+            </VStack>
+          </ModalBody>
+
+          <ModalFooter alignItems="center" >
+            <Button colorScheme="blue" ml="auto" mr="auto" onClick={handleFormSubmission}>
+              Create Group
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  )
+}
+
+export default AddGroupModal;
---
+++ src/Groups/JoinGroupModal.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import { useDisclosure } from "@chakra-ui/react";
+import { Input } from "@chakra-ui/react";
+import { VStack } from "@chakra-ui/react";
+import { Button } from "@chakra-ui/react";
+import { FormLabel, FormControl } from '@chakra-ui/react';
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+} from "@chakra-ui/react";
+
+function JoinGroupModal() {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const [groupCode, setGroupCode] = useState();
+
+  const handleGroupCodeChange = (e) => {
+    let groupCode = e.target.value;
+    setGroupCode(groupCode);
+  };
+
+  const handleFormSubmission = (e) => {
+    onClose()
+    // HANDLE FORM SUBMISSION
+  }
+
+
+  return (
+    <>
+      <Button onClick={onOpen}>Join Existing Group</Button>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader >Join Existing Group</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <VStack spacing="20px">
+              <FormControl isRequired>
+                <FormLabel>Group Invitation Code</FormLabel>
+                <Input
+                  value={groupCode}
+                  onChange={handleGroupCodeChange}
+                  placeholder="Invitation Code"
+                />
+              </FormControl>
+            </VStack>
+          </ModalBody>
+
+          <ModalFooter alignItems="center" >
+            <Button colorScheme="blue" ml="auto" mr="auto" onClick={handleFormSubmission}>
+              Join Group
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  )
+}
+
+export default JoinGroupModal;
