4a37d5242e6764fb3ca5626ae33a578507a9e823
Eric Handley <ehandley96@gmail.com>
2021-07-12 11:11:50 -0500

    Added Login component. Changed server port to 3001.

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const db = require('../database');
-const port = 3000;
+const port = 3001;
 
 const app = express();
 
@@ -9,6 +9,6 @@ app.use(express.static(path.join(__dirname, './public')));
 app.use(express.json());
 
 app.listen(port, (err) => {
-  if(err) console.log(err);
+  if (err) console.log(err);
   console.log(`Listening to port ${port}`);
 });
