04327d9f65a7d12aa8623e4ef6ab8821ae6afbcc
Angela Wu <ange.y.wu@gmail.com>
2021-07-13 14:19:58 -0500

    tweaks to members and recs

--- src/members/Members.js
+++ src/members/Members.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 
-import { Box, Text, Heading, SimpleGrid, IconButton, Flex, Spacer, Avatar } from "@chakra-ui/react"
+import { Box, Center, Text, Heading, SimpleGrid, IconButton, Flex, Spacer, Avatar } from "@chakra-ui/react"
 import { TriangleUpIcon, TriangleDownIcon, EmailIcon } from '@chakra-ui/icons'
 
 //need to pass as props
@@ -55,82 +55,28 @@ export default function Members(props) {
 
   return (
     <Box maxW="100%" width="100%" borderWidth="1px" borderRadius="lg" overflow="hidden">
-      {/* <Flex p="4">
-        <Spacer />
-        <Spacer />
-        <Spacer />
-        <Spacer />
-        <Spacer />
-        <Spacer />
-        <Spacer /> */}
       <Box p="1">
         <Heading size="lg">Group Members</Heading>
       </Box>
-      {/* <Spacer />
-        <Spacer />
-        <Spacer />
-        <Spacer />
-        <Spacer />
-        <Spacer />
-        <Spacer /> */}
       <Box >
         <Text p="1" fontSize="sm"> Invite Friends with Group Code: {code}</Text>
         {/* <IconButton onClick={onInvite} aria-label="Invite" p="2" size="sm" icon={<EmailIcon boxSize={6} />} /> */}
-
       </Box>
-      {/* <Spacer />
-      </Flex> */}
       <SimpleGrid columns={2}>
         {members.slice(0, itemsShown).map((each, i) =>
           eachMember(each, i)
         )}
       </SimpleGrid>
-      {
-        itemsShown < members.length && members.length > 6 ?
-          <IconButton onClick={onSeeMore} aria-label="See More" icon={<TriangleDownIcon />} />
-          :
-          <IconButton onClick={onCollapse} aria-label="Collapse" icon={<TriangleUpIcon />} />
-      }
-      <br />
+      <Center>
+        {
+          itemsShown < members.length && members.length > 6 ?
+            <IconButton onClick={onSeeMore} aria-label="See More" icon={<TriangleDownIcon />} />
+            :
+            <IconButton onClick={onCollapse} aria-label="Collapse" icon={<TriangleUpIcon />} />
+        }
+        <br /><br />
+      </Center>
     </Box >
   )
 
-
-
-
-  //     {category === "All" ?
-  //       <SimpleGrid columns={2} >
-  //         {apiTransformed.slice(0, itemsShown).map((each, i) =>
-  //           eachRecommendation(each, i)
-  //         )}
-  //       </SimpleGrid>
-  //       :
-  //       <SimpleGrid columns={2} >
-  //         {/*  */}
-  //         {/* Consider making separate data arrays for each category? */}
-  //         {/* Problem is that this is truncating results */}
-  //         {/*  */}
-  //         {apiTransformed.slice(0, itemsShown).map((each, i) => {
-  //           if (category === each.category) {
-  //             return (
-  //               eachRecommendation(each, i)
-  //             )
-  //           }
-  //         }
-  //         )}
-  //       </SimpleGrid>
-  //     }
-  //     <Center>
-
-  //       {
-  //         itemsShown < apiTransformed.length && apiTransformed.length > 4 ?
-  //           <IconButton onClick={onSeeMore} aria-label="See More" icon={<TriangleDownIcon />} />
-  //           :
-  //           <IconButton onClick={onCollapse} aria-label="Collapse" icon={<TriangleUpIcon />} />
-  //       }
-
-  //       <br /><br />
-  //     </Center>
-  //   </Box >
-  // )
 }
