6848a55741a23872ce191ef215b2bb42fe138426
Angela Wu <ange.y.wu@gmail.com>
2021-07-13 12:30:51 -0500

    skeleton complete

--- src/components/members/Members.js
+++ src/components/members/Members.js
@@ -1,22 +1,22 @@
 import React, { useState, useEffect } from 'react';
 
-import { Box } from "@chakra-ui/react"
-import { Text } from "@chakra-ui/react"
-import { Heading } from "@chakra-ui/react"
-import { Image } from "@chakra-ui/react"
-import { SimpleGrid } from "@chakra-ui/react"
-import { Select } from "@chakra-ui/react"
-import { Center } from "@chakra-ui/react"
-import { TriangleUpIcon, TriangleDownIcon } from '@chakra-ui/icons'
-import { IconButton } from "@chakra-ui/react"
+import { Box, Text, Heading, SimpleGrid, IconButton, Flex, Spacer, Avatar } from "@chakra-ui/react"
+import { TriangleUpIcon, TriangleDownIcon, EmailIcon } from '@chakra-ui/icons'
 
 //need to pass as props
-const members = [{ firstName: 'Jack', lastName: 'Pronske' }, { firstName: 'Joe', lastName: 'Haller' }, { firstName: 'Cody', lastName: 'Haines' }, { firstName: 'Christian', lastName: 'Peterson' }]
+const members = [{ firstName: 'Jack', lastName: 'Pronske' }, { firstName: 'Kim', lastName: 'Kost' },
+{ firstName: 'Tom', lastName: 'Chandler' }, { firstName: 'Allison', lastName: 'Dillon' },
+{ firstName: 'Joe', lastName: 'Haller' }, { firstName: 'Cody', lastName: 'Haines' },
+{ firstName: 'Christian', lastName: 'Peterson' }]
 
 export default function Members(props) {
 
-  const [itemsShown, setItemsShown] = useState(6);
+  const [itemsShown, setItemsShown] = useState(4);
   const [isMore, setIsMore] = useState(true);
+
+  const onInvite = () => {
+    //
+  };
 
   const onSeeMore = () => {
     setItemsShown(itemsShown + 4)
@@ -27,21 +27,54 @@ export default function Members(props) {
   };
 
   const eachMember = (dataObj, index) => {
+    let fullName = dataObj.firstName + " " + dataObj.lastName;
+
     return (
       < Box key={index} p="2" >
-        <Box
-          mt="1"
-          lineHeight="tight"
-        >
-          <Text fontSize="s">{dataObj.firstName} {dataObj.lastName.slice(0, 1)}.</Text>
-        </Box>
+        <Flex p="4">
+          <Spacer />
+          {/* Future Feature */}
+          {/* Can add avatar image URL to src attribute */}
+          <Avatar size="lg" name={fullName} src="" />
+          <Box
+            p="4"
+            mt="1"
+            lineHeight="tight"
+          >
+            <Text fontSize="s">{dataObj.firstName} {dataObj.lastName.slice(0, 1)}.</Text>
+          </Box>
+          <Spacer />
+          <Spacer />
+          <Spacer />
+        </Flex>
       </Box>
     )
   }
 
   return (
     <Box maxW="100%" width="100%" borderWidth="1px" borderRadius="lg" overflow="hidden">
-      <Heading size="lg">Group Members</Heading>
+      <Flex p="4">
+        <Spacer />
+        <Spacer />
+        <Spacer />
+        <Spacer />
+        <Spacer />
+        <Spacer />
+        <Spacer />
+        <Box p="1">
+          <Heading size="lg">Group Members</Heading>
+        </Box>
+        <Spacer />
+        <Spacer />
+        <Spacer />
+        <Spacer />
+        <Spacer />
+        <Spacer />
+        <Spacer />
+        <Text fontSize="sm" p="2"> Invite A Friend</Text>
+        <IconButton onClick={onInvite} aria-label="Invite" p="2" size="sm" icon={<EmailIcon boxSize={6} />} />
+        <Spacer />
+      </Flex>
       <SimpleGrid columns={2}>
         {members.slice(0, itemsShown).map((each, i) =>
           eachMember(each, i)
@@ -55,7 +88,7 @@ export default function Members(props) {
           <IconButton onClick={onCollapse} aria-label="Collapse" icon={<TriangleUpIcon />} />
       }
       <br /><br />
-    </Box>
+    </Box >
   )
 
 
