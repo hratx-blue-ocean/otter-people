ec2696109bbb55932cb1566b966db7e4883fdbf5
Felipe Erazo <felipe.erazoangarita@gmail.com>
2021-07-15 15:28:38 -0500

    Starting to style events column

--- src/groupAndEvents/SelectedGroup.js
+++ src/groupAndEvents/SelectedGroup.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import AddEventModal from './AddEventModal';
-import { Box, Center, Grid, GridItem, Button, ButtonGroup, Text, Heading, SimpleGrid, IconButton, Flex, Spacer, Avatar } from "@chakra-ui/react"
+import { useColorModeValue, Box, Center, Grid, GridItem, Button, ButtonGroup, Text, Heading, SimpleGrid, IconButton, Flex, Spacer, Avatar } from "@chakra-ui/react"
 import { StarIcon } from '@chakra-ui/icons'
 
 //need to pass as props
@@ -12,8 +12,14 @@ export default function SelectedGroup(props) {
     //need to create drawer
   }
 
+  // Colors
+  const layer = useColorModeValue('layer.light', 'layer.dark');
+  const gBtn = useColorModeValue('gBtn.light', 'gBtn.dark');
+  const mainBlue = useColorModeValue('mainBlue.light', 'mainBlue.dark');
+  const txt = useColorModeValue('text.light', 'text.dark');
+
   return (
-    <Box maxW="90%" width="90%" height="140px" borderBottomWidth="1px" borderRadius="sm" >
+    <Box maxW="90%" width="90%" height="140px" borderBottomWidth="1px" borderRadius="sm" bg={layer} borderColor={layer} >
       <Flex >
         <Spacer />
         <StarIcon mt="10" w={12} h={12} />
