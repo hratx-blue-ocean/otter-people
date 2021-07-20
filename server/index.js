04aae1ad1228ce8d0a2046e99562935334d5e935
Felipe Erazo <felipe.erazoangarita@gmail.com>
2021-07-20 11:36:37 -0500

    change port

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const cors = require('cors');
 const db = require('../database');
-const port = 3001;
+const port = 3000;
 
 const app = express();
 app.use(cors());
